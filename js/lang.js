60bef5292be06316825be8edec241be200e823cf
unknown <mariam-haris>
2019-05-27 12:20:04 +0200

    project upload

---
+++ js/lang.js
@@ -0,0 +1,337 @@
+function getLangResources(){
+    
+    // Define arrays how many language you want to translate
+
+    var ar = new Array();
+    var en = new Array();
+
+    ar['arabicBtn'] = "ع"; 
+    en['arabicBtn'] = "ar";
+
+    // NAVBAR
+
+    ar['navHome'] = "الرئيسية"; 
+    en['navHome'] = "HOME";
+    
+    ar['aboutLi'] = "من نحن"; 
+    en['aboutLi'] = "ABOUT US";
+
+    ar['pagesLi'] = "الصفحات"; 
+    en['pagesLi'] = "PAGES";
+
+    ar['projectsLi'] = "المشروعات"; 
+    en['projectsLi'] = "PROJECTS";
+
+    ar['blogLi'] = "المدونة"; 
+    en['blogLi'] = "BLOG";
+
+    ar['contactLi'] = "اتصل بنا"; 
+    en['contactLi'] = "CONTACT US";
+
+    ar['language'] = "اللغة"; 
+    en['language'] = "LANGUAGE";
+    
+    ar['navDate'] = "الاثنين - السبت :7:00 - 17:00 "; 
+    en['navDate'] = "Mon - Sat: 7:00 - 17:00";
+    
+
+    // HEADER
+
+    ar['sliderTitle'] = "نحن هنا من اجلك"; 
+    en['sliderTitle'] = "We care about your business";
+
+    ar['sliderSubTitle'] = "نحن هنا من اجلك    إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات كما تريد، النص لن يبدو مقسما ولا يحوي أخطاء لغوية، مولد النص العربى مفيد لمصممي المواقع على وجه الخصوص، حيث يحتاج العميل فى كثير من الأحيان أن يطلع على صورة حقيقية لتصميم الموقع."; 
+    en['sliderSubTitle'] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam elementum consectetur egestas. Nulla nisi nisi, rutrum vitae nunc non, bibendum euismod nulla";
+
+    ar['sliderBtn'] = "اضغط هنا"; 
+    en['sliderBtn'] = "about us";
+
+    // FEATURES
+
+    ar['featureTitle'] = "منتاجتنا";
+    en['featureTitle'] = "products"; 
+
+
+    ar['featureDetails'] = "نحن هنا من اجل نعم إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات ";
+    en['featureDetails'] = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus, aliquid vel? Sunt facere dolorem laudantium?";
+
+    ar['sliderBtn'] = "عننا"; 
+    en['sliderBtn'] = "about us";
+
+    //PRODUCTS
+
+    ar['productTitle1'] = "المنتجات";
+    en['productTitle1'] = "Nibh Ridiculus"; 
+   
+    ar['productTitle2'] = "الفقرات";
+    en['productTitle2'] = "grammatica del"; 
+
+    ar['productTitle3'] = " المشروعات";
+    en['productTitle3'] = "Europan lingues"; 
+
+    ar['productTitle4'] = "الاشكال";
+    en['productTitle4'] = "ipsum quia"; 
+
+    ar['productTitle5'] = "النصوص";
+    en['productTitle5'] = "aliquam quaerat"; 
+
+    ar['productTitle6'] = "الصفحات";
+    en['productTitle6'] = "dolorem eum"; 
+
+    ar['productBody'] = "هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها.َ ";
+    en['productBody'] = "Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus etmagnis dis parturie nt montes, nascetur."; 
+
+    // PROJECTS
+
+    ar['projectTitle'] = "اخر المشروعات";
+    en['projectTitle'] = "RECENT PROJECTS";
+
+    // COUNTER SECTION
+
+    ar['counterNum'] = "٤٥٠";
+    en['counterNum'] = "450";
+
+    ar['counterTitle'] = "العملاء السعداء";
+    en['counterTitle'] = "happy clients";
+
+    // BLOG SECTION
+
+    ar['blogTitle'] = "من المدونة";
+    en['blogTitle'] = "FORM BLOG";
+
+    ar['blogItemDate'] = "٢٣ اكتوبر ٢٠١٩";
+    en['blogItemDate'] = "23 oct 2019";
+
+    ar['blogItemDetails'] = "نحن هنا من اجل نعم إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات ";
+    en['blogItemDetails'] = " Dummy text is text that is used in theublish industry or by web designers to occupy the space which will later be";
+
+    ar['blogItemBtn'] = "اقرأ المزيد";
+    en['blogItemBtn'] = "READ MORE";
+
+    //IN-TOUCH SECTION
+
+    ar['inTochTitle'] = "اسأل ما تريد نحن دائما هنا لمساعدتك";
+    en['inTochTitle'] = "GO AHEAD AND FIND OUT HOW WE CAN HELP YOU!";
+
+    ar['inTochBtn'] = "ابقى على تواصل";
+    en['inTochBtn'] = "GET IN TOUCH";
+
+    //FOOTER SECTION
+
+    ar['footerAboutTitle'] = "من نحن";
+    en['footerAboutTitle'] = "About Us";
+
+    ar['footerAboutDetails'] = "نحن هنا من اجل نعم إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد النص العربى زيادة عدد الفقرات ";
+    en['footerAboutDetails'] = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab voluptates impedit labore sapiente earum fugiat vero dolorum cupiditate, deserunt reprehenderit.";
+
+    ar['footerLinkesTitle'] = "روابط المنتدى";
+    en['footerLinkesTitle'] = "Related Links";
+
+    ar['footerLinkesnum1'] = "روابط المنتدى";
+    en['footerLinkesnum1'] = "Official Site";
+
+    ar['footerLinkesnum2'] = "صفحة السوق";
+    en['footerLinkesnum2'] = "Creative Market profile";
+
+    ar['footerLinkesnum3'] = "المحتوى المقروء";
+    en['footerLinkesnum3'] = "Materialzer page on CM";
+
+    ar['footerLinkesnum4'] = "محتوى نصي";
+    en['footerLinkesnum4'] = "Electronic WP Theme";
+
+    ar['footerLinkesnum5'] = "العبارات الفكاهية";
+    en['footerLinkesnum5'] = "Was favvvv";
+    
+    ar['footerLinkeOne'] = "مدونة";
+    en['footerLinkeOne'] = "Blog";
+
+    ar['footerLinkeTwo'] = "صفحة";
+    en['footerLinkeTwo'] = "pages";
+
+    ar['footerNewsTitle'] = "اخر الاخبار";
+    en['footerNewsTitle'] = "news & media";
+
+    ar['footerNewsnum1'] = "الاحداث";
+    en['footerNewsnum1'] = "Events";
+
+    ar['footerNewsnum2'] = "الاخبار";
+    en['footerNewsnum2'] = "News and Press Releases";
+
+    ar['footerNewsnum3'] = "المقابلات";
+    en['footerNewsnum3'] = "Interviews";
+
+    ar['footerNewsnum4'] = "جهات الاتصال";
+    en['footerNewsnum4'] = "Subsidiaries Media Contacts";
+
+    ar['footerNewsnum5'] = "الاعلام";
+    en['footerNewsnum5'] = "Media Kit";
+    
+    ar['footerTouchTitle'] = "ابقى على تواصل";
+    en['footerTouchTitle'] = "Get in touch";
+
+    ar['footerTouchDetails'] = "نحن هنا من اجل نعم إذا كنت تحتاج إلى عدد أكبر من الفقرات يتيح لك مولد ";
+    en['footerTouchDetails'] = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, voluptates!";
+
+    ar['footerTouchAddress'] = "الدولة , الشارع , المدينة";
+    en['footerTouchAddress'] = "84 Street, City, State 24813";
+
+
+
+
+
+    // CONTACT-US PAGE TRANSLATION
+
+    ar["firstName"] = "الاسم الاول";
+    en['firstName'] = "First Name"; 
+    
+    ar['firstName'] = "الاسم الاول";
+    en['firstName'] = "";
+
+    ar['lastName'] = "الاسم الثانى";
+    en['lastName'] = "";
+
+    ar['email'] = "الايميل الالكترونى";
+    en['email'] = "";
+
+    ar['website'] = "موقعك الالكترونى";
+    en['website'] = "";
+
+    ar['yourMessage'] = "ارسل رسالتك";
+    en['yourMessage'] = "";
+
+    ar['submitBtn'] = "ارسل";
+    en['submitBtn'] = "send";
+
+    ar['homeContactUs'] = "الرئيسية > اتصل بنا ";
+    en['homeContactUs'] = "Home > Contact us";
+
+   
+    
+
+
+    // NEWS PAGE TRANSLATION
+
+    ar['homeNews'] = "الرئيسية > الاخبار ";
+    en['homeNews'] = " Home > News";
+
+    ar['newsMainTitle'] =  "خطوات نحو تطوير استراتيجية المحتوى  ";
+    en['newsMainTitle'] = " Steps Toward Developing A Content Strategy";
+
+    ar['newsMainBody'] =  "نالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم، ولكن الغالبية تم تعديلها بشكل ما عبر إدخال بعض النوادر أو الكلمات العشوائية إلى النص. إن كنت تريد أن تستخدم نص لوريم إيبسوم ما، عليك أن تتحقق أولاً أن ليس هناك أي كلمات أو عبارات محرجة أو غير لائقة مخبأة في هذا النص. بينما تعمل جميع مولّدات نصوص لوريم إيبسوم على الإنترنت على إعادة تكرار مقاطع من نص لوريم إيبسوم نفسه عدة مرات بما تتطلبه الحاجة . ";
+    en['newsMainBody'] = " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab debitis ea est illum ipsa itaque liberop tiovolup Conseq uuntur deserunt expedita, fugiat hic illum porro quidem quis recusandae vero? Aliquiassumenda cum delectus eaque eligendi, enim eum excepturi fugit illum impedit in iste laudantium modi natus, nisi nobis gfy g obcaecati praesentium quis reiciendis rerum sapiente ";
+
+    ar['postsTitle'] =  "البوستات الاخيرة  ";
+    en['postsTitle'] = " Recent Posts";
+
+    ar['newsBlogTitle'] =  "نحن هنا من اجل نعم إذا كنت تحتاج إلى عدد أكبر من الفقرات  ";
+    en['newsBlogTitle'] = " There is no good blog without tim great readability is very importent";
+   
+    ar['newsBlogNote'] =  "التي يمكنك ان تراها  ";
+    en['newsBlogNote'] = " Viral polaroid ugh";
+    
+    ar['projectTitle'] =  "المشروعات الاخيرة  ";
+    en['projectTitle'] = " Recent Projects";
+    
+    ar['blogCategorieTitle'] =  "الكتالوجات  ";
+    en['blogCategorieTitle'] = " Blog Categories";
+
+    ar['blogCategorie1'] =  "المباني  ";
+    en['blogCategorie1'] = " Building";
+
+    ar['blogCategorie2'] =  "الشركة  ";
+    en['blogCategorie2'] = " company";
+
+    ar['blogCategorie3'] =  "التصميم  ";
+    en['blogCategorie3'] = " Design";
+
+    ar['blogCategorie4'] =  "الاعلام ";
+    en['blogCategorie4'] = " Press";
+
+    ar['blogCategorie5'] =  "الخطوات  ";
+    en['blogCategorie5'] = " Tips";
+
+    ar['date'] =  "التاريخ  ";
+    en['date'] = " Date";
+
+
+    
+    
+    // Added new array defined arrays.
+    var resources = new Array();
+    resources['ar'] = ar;
+    resources['en'] = en;
+    
+    return resources;
+}
+
+function changeLanguage(lng){
+var resources = getLangResources()[lng];
+
+$("a[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+$("ul[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+$("li[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+$("h2[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+$("h3[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+$("h4[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+$("h5[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+$("h6[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+$("p[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+$("button[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+$("span[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+$("label[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+$("input[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+$("placeholder[name='translate']").each(function(i, elt){
+    $(elt).text(resources[$(elt).attr("caption")]);
+});
+}
+
+
+
+
+$(function() { 
+    // Default Language
+    changeLanguage("en");
+ 
+    $("#ar_button").click(function(){
+        changeLanguage("ar");
+        $("html").attr("dir", function(){
+            $("html").attr("dir", "rtl");
+            $("body").css("text-align","right");
+            $(".icon-search").css({"left":"5%", "right":"unset"})
+        });
+    });
+
+    $("#eng_button").click(function(){
+        changeLanguage("en");
+        $("html").attr("dir", function(){
+            $("html").attr("dir", "ltr");
+            $("body").css("text-align","left");
+        });
+    });
+});
